eec4b8867853d7f41924a2c4f62b5521c5884de6
Ankit Singh <66105983+ankitzm@users.noreply.github.com>
2025-10-25 13:43:26 +0530

    fix: output dir and internal packages fixed

--- wxt.config.ts
+++ wxt.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
+  browser: "chrome",
   modules: ["@wxt-dev/module-react"],
+  outDir: "output",
   webExt: {
     startUrls: ["https://google.com"],
+    disabled: true,
+    binaries: { chrome: "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser" },
   },
 });
