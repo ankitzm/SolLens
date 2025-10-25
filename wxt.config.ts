cf3a924d77ae4f843f9a56f3468d6900a22537d4
Ankit Singh <66105983+ankitzm@users.noreply.github.com>
2025-10-25 23:06:38 +0530

    fix: minor config fix

--- wxt.config.ts
+++ wxt.config.ts
@@ -5,9 +5,16 @@ export default defineConfig({
   browser: "chrome",
   modules: ["@wxt-dev/module-react"],
   outDir: "output",
+  manifest: {
+    name: "WNA - Wallet Namer",
+    description: "Locally name Solana wallet addresses on Solscan",
+    version: "0.0.1",
+    permissions: ["contextMenus", "storage", "activeTab"],
+    host_permissions: ["https://solscan.io/*"],
+  },
   webExt: {
-    startUrls: ["https://google.com"],
-    disabled: true,
+    startUrls: ["https://solscan.io"],
+    disabled: false,
     binaries: { chrome: "/Applications/Brave Browser.app/Contents/MacOS/Brave Browser" },
   },
 });
